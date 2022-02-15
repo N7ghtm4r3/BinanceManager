921b279a5103a3859317be3d146ce7e05857bb61
N7ghtm4r3 <manuel033689@gmail.com>
2022-02-15 12:07:30 +0100

    Load first files

---
+++ src/main/java/com/tecknobit/binancemanager/Managers/BinanceManager.java
@@ -0,0 +1,69 @@
+package com.tecknobit.binancemanager.Managers;
+
+import com.tecknobit.binancemanager.Exceptions.SystemException;
+import com.tecknobit.binancemanager.Helpers.ConnectionManager;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.tecknobit.binancemanager.Constants.EndpointsList.SYSTEM_STATUS_URL;
+import static com.tecknobit.binancemanager.Constants.EndpointsList.TIMESTAMP_URL;
+import static com.tecknobit.binancemanager.Helpers.ConnectionManager.GET_METHOD;
+import static java.lang.System.currentTimeMillis;
+import static java.util.Arrays.asList;
+
+public class BinanceManager {
+
+    public static final ArrayList<String> BASE_ENDPOINTS = new ArrayList<>(asList("https://api.binance.com",
+            "https://api1.binance.com","https://api2.binance.com","https://api3.binance.com"));
+    protected ConnectionManager connectionManager;
+    protected JSONObject jsonObject;
+    protected JSONArray jsonArray;
+    protected final String baseEndpoint;
+
+    /** Constructor to init all BinanceManagers
+     * @param #baseEndpoint base endpoint to work on
+     * **/
+    public BinanceManager(String baseEndpoint) throws SystemException, IOException {
+        if(baseEndpoint != null)
+            this.baseEndpoint = baseEndpoint;
+        else
+            this.baseEndpoint = getDefaultBaseEndpoint();
+        this.connectionManager = new ConnectionManager();
+    }
+
+    /** Method to set automatically a working endpoint
+     * any params required
+     * **/
+    protected String getDefaultBaseEndpoint() throws SystemException, IOException {
+        for (String string : BASE_ENDPOINTS)
+            if(isSystemAvailable(string))
+                return string;
+        throw new SystemException();
+    }
+
+    /** Request to get system status
+     * @param #baseEndpoint endpoint to request status
+     * **/
+    public boolean isSystemAvailable(String baseEndpoint) throws IOException {
+        connectionManager.startConnection(baseEndpoint+SYSTEM_STATUS_URL,GET_METHOD);
+        jsonObject = new JSONObject(connectionManager.getResponse());
+        return jsonObject.getInt("status") == 0;
+    }
+
+    /** Request to get server timestamp or your current timestamp
+     * any param required
+     * **/
+    public long getTimestamp(){
+        try {
+            connectionManager.startConnection(baseEndpoint+TIMESTAMP_URL,GET_METHOD);
+            jsonObject = new JSONObject(connectionManager.getResponse());
+            return jsonObject.getLong("serverTime");
+        } catch (IOException e) {
+            return currentTimeMillis();
+        }
+    }
+
+}
---
+++ src/main/java/com/tecknobit/binancemanager/Managers/BinanceWalletManager.java
@@ -0,0 +1,52 @@
+package com.tecknobit.binancemanager.Managers;
+
+import com.tecknobit.binancemanager.Exceptions.SystemException;
+
+import java.io.IOException;
+
+import static com.tecknobit.binancemanager.Constants.EndpointsList.DAILY_ACCOUNT_SNAP_URL;
+import static com.tecknobit.binancemanager.Helpers.ConnectionManager.GET_METHOD;
+
+public class BinanceWalletManager extends BinanceManager{
+
+    //https://binance-docs.github.io/apidocs/spot/en/#general-api-information
+    public static final String SPOT = "SPOT";
+    public static final String MARGIN = "MARGIN";
+    public static final String FUTURES = "FUTURES";
+    private final String apiKey;
+    private final String secretKey;
+
+    /** Constructor with an endpoint give by parameter
+     * @param #apiKey your api key
+     * @param #secretKey your secret key
+     * @param #baseEndpoint base endpoint choose from BASE_ENDPOINTS array
+     * **/
+    public BinanceWalletManager(String apiKey, String secretKey, String baseEndpoint) throws IOException, SystemException {
+        super(baseEndpoint);
+        this.apiKey = apiKey;
+        this.secretKey = secretKey;
+        if(!isSystemAvailable(baseEndpoint))
+            throw new SystemException();
+    }
+
+    /** Constructor with an endpoint give by list research
+     * @param #apiKey your api key
+     * @param #secretKey your secret key
+     * **/
+    public BinanceWalletManager(String apiKey, String secretKey) throws SystemException, IOException {
+        super(null);
+        this.apiKey = apiKey;
+        this.secretKey = secretKey;
+    }
+
+    /** Request to get your daily account snapshot
+     * @param #type: SPOT,MARGIN OR FUTURES
+     * **/
+    public String getAccountSnapshot(String type) throws Exception {
+        String params = "?timestamp="+getTimestamp()+"&type="+type;
+        connectionManager.startConnection(baseEndpoint+DAILY_ACCOUNT_SNAP_URL+params+"&signature="
+                +connectionManager.getSignature(secretKey,params),GET_METHOD,apiKey);
+        return connectionManager.getResponse();
+    }
+
+}
