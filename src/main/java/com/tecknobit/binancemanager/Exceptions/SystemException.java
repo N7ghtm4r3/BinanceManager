153ce78b3767579a48ba0875c1f3fd1147249a0d
N7ghtm4r3 <manuel033689@gmail.com>
2022-02-22 11:05:28 +0100

    Load first files

--- src/main/java/com/tecknobit/binancemanager/Exceptions/SystemException.java
+++ src/main/java/com/tecknobit/binancemanager/Exceptions/SystemException.java
@@ -1,11 +1,12 @@
 package com.tecknobit.binancemanager.Exceptions;
 
-public class SystemException extends Exception{
+/**
+ *  The {@code SystemException} class is useful to catch if Binance API service is working or not,
+ *  it throws an exception if service is not working.
+ * **/
 
-    /**
-     *  The {@code SystemException} class is useful to catch if Binance API service is working or not,
-     *  it throws an exception if service is not working.
-     * **/
+
+public class SystemException extends Exception{
 
     public SystemException() {
         super("Service unavailable because Binance system is in maintenance");
