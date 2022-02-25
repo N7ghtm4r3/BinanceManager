3b1c77e059886f84e13cc6261163da7ea154ff23
N7ghtm4r3 <manuel033689@gmail.com>
2022-02-25 16:51:11 +0100

    Load first files

--- src/main/java/com/tecknobit/binancemanager/Managers/SignedManagers/Trade/Spot/Records/Orders/OpenOrder.java
+++ src/main/java/com/tecknobit/binancemanager/Managers/SignedManagers/Trade/Spot/Records/Orders/OpenOrder.java
@@ -1,12 +1,7 @@
 package com.tecknobit.binancemanager.Managers.SignedManagers.Trade.Spot.Records.Orders;
 
-public class OpenOrder extends CancelOrder{
+public class OpenOrder {
 
-    public OpenOrder(String symbol, String origClientOrderId, long orderId, long orderListId, String clientOrderId,
-                     double price, double origQty, double executedQty, double cummulativeQuoteQty, String status,
-                     String timeInForce, String type, String side) {
-        super(symbol, origClientOrderId, orderId, orderListId, clientOrderId, price, origQty, executedQty,
-                cummulativeQuoteQty, status, timeInForce, type, side);
-    }
+    
 
 }
