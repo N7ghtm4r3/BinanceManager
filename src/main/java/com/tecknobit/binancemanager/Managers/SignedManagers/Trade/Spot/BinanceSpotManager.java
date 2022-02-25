961058e354c7aa6bda2a68808d8e77910cc4322f
N7ghtm4r3 <manuel033689@gmail.com>
2022-02-25 15:50:50 +0100

    Load first files

--- src/main/java/com/tecknobit/binancemanager/Managers/SignedManagers/Trade/Spot/BinanceSpotManager.java
+++ src/main/java/com/tecknobit/binancemanager/Managers/SignedManagers/Trade/Spot/BinanceSpotManager.java
@@ -2,6 +2,10 @@ package com.tecknobit.binancemanager.Managers.SignedManagers.Trade.Spot;
 
 import com.tecknobit.binancemanager.Exceptions.SystemException;
 import com.tecknobit.binancemanager.Managers.SignedManagers.BinanceSignedManager;
+import com.tecknobit.binancemanager.Managers.SignedManagers.Trade.Spot.Records.Orders.ACKOrder;
+import com.tecknobit.binancemanager.Managers.SignedManagers.Trade.Spot.Records.Orders.FullOrder;
+import com.tecknobit.binancemanager.Managers.SignedManagers.Trade.Spot.Records.Orders.ResultOrder;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -9,6 +13,8 @@ import java.util.HashMap;
 import static com.tecknobit.binancemanager.Constants.EndpointsList.SEND_NEW_ORDER_ENDPOINT;
 import static com.tecknobit.binancemanager.Constants.EndpointsList.TEST_NEW_ORDER_ENDPOINT;
 import static com.tecknobit.binancemanager.Helpers.Request.RequestManager.POST_METHOD;
+import static com.tecknobit.binancemanager.Managers.SignedManagers.Trade.Spot.Records.Orders.ACKOrder.*;
+import static com.tecknobit.binancemanager.Managers.SignedManagers.Trade.Spot.Records.Orders.ACKOrder.LIMIT;
 
 /**
  *  The {@code BinanceSpotManager} class is useful to manage all Binance Spot Endpoints
@@ -55,15 +61,84 @@ public class BinanceSpotManager extends BinanceSignedManager {
         return newOrderRequest(SEND_NEW_ORDER_ENDPOINT,requestManager.assembleExtraParams(params,extraParams));
     }
 
-    public String sendNewOrder(String symbol, String side, String type,String newOrderRespType,
+    public String sendNewOrder(String symbol, String side, String type, String newOrderRespType,
                                HashMap<String,Object> extraParams) throws Exception {
         String params = getParamTimestamp()+"&symbol="+symbol+"&side="+side+"&type="+type+"&newOrderRespType="+newOrderRespType;
         return newOrderRequest(SEND_NEW_ORDER_ENDPOINT,requestManager.assembleExtraParams(params,extraParams));
     }
 
+    public JSONObject sendJSONNewOrder(String symbol, String side, String type,HashMap<String,Object> extraParams) throws Exception {
+        return new JSONObject(sendNewOrder(symbol,side,type,extraParams));
+    }
+
+    public JSONObject sendJSONNewOrder(String symbol, String side, String type, String newOrderRespType,
+                                       HashMap<String,Object> extraParams) throws Exception {
+        return new JSONObject(sendNewOrder(symbol,side,type,newOrderRespType,extraParams));
+    }
+
+    public ACKOrder sendObjectNewOrder(String symbol, String side, String type,HashMap<String,Object> extraParams) throws Exception {
+        jsonObject = new JSONObject(sendNewOrder(symbol,side,type,extraParams));
+        if(type.equals(LIMIT) || type.equals(MARKET))
+            return getFullOrderResponse(jsonObject);
+        else
+            return getACKResponse(jsonObject);
+    }
+
+    public ACKOrder sendObjectNewOrder(String symbol, String side, String type,String newOrderRespType,
+                                       HashMap<String,Object> extraParams) throws Exception {
+        jsonObject = new JSONObject(sendNewOrder(symbol,side,type,newOrderRespType,extraParams));
+        switch (newOrderRespType){
+            case NEW_ORDER_RESP_TYPE_RESULT:
+                return new ResultOrder(jsonObject.getString("symbol"),
+                        jsonObject.getLong("orderId"),
+                        jsonObject.getLong("orderListId"),
+                        jsonObject.getString("clientOrderId"),
+                        jsonObject.getLong("transactTime"),
+                        jsonObject.getDouble("price"),
+                        jsonObject.getDouble("origQty"),
+                        jsonObject.getDouble("executedQty"),
+                        jsonObject.getDouble("cummulativeQuoteQty"),
+                        jsonObject.getString("status"),
+                        jsonObject.getString("timeInForce"),
+                        jsonObject.getString("type"),
+                        jsonObject.getString("side")
+                );
+            case NEW_ORDER_RESP_TYPE_FULL:
+                return getFullOrderResponse(jsonObject);
+            default:
+                return getACKResponse(jsonObject);
+        }
+    }
+
+    private ACKOrder getACKResponse(JSONObject response){
+        return new ACKOrder(response.getString("symbol"),
+                response.getLong("orderId"),
+                response.getLong("orderListId"),
+                response.getString("clientOrderId"),
+                response.getLong("transactTime")
+        );
+    }
+
+    private FullOrder getFullOrderResponse(JSONObject response){
+        return new FullOrder(response.getString("symbol"),
+                response.getLong("orderId"),
+                response.getLong("orderListId"),
+                response.getString("clientOrderId"),
+                response.getLong("transactTime"),
+                response.getDouble("price"),
+                response.getDouble("origQty"),
+                response.getDouble("executedQty"),
+                response.getDouble("cummulativeQuoteQty"),
+                response.getString("status"),
+                response.getString("timeInForce"),
+                response.getString("type"),
+                response.getString("side"),
+                response.getJSONArray("fills")
+        );
+    }
+
     private String newOrderRequest(String endpoint,String params) throws Exception {
         return getRequestResponse(endpoint,params+getSignature(params),POST_METHOD,apiKey);
     }
-
 
 }
--- src/main/java/com/tecknobit/binancemanager/Managers/SignedManagers/Trade/Spot/Records/Orders/FullOrder.java
+++ src/main/java/com/tecknobit/binancemanager/Managers/SignedManagers/Trade/Spot/Records/Orders/FullOrder.java
@@ -1,25 +1,79 @@
 package com.tecknobit.binancemanager.Managers.SignedManagers.Trade.Spot.Records.Orders;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class FullOrder extends ResultOrder{
 
-    private final ArrayList<Fill> fills;
+    private ArrayList<Fill> fills;
 
     public FullOrder(String symbol, long orderId, long orderListId, String clientOrderId, long transactTime, double price,
                      double origQty, double executedQty, double cummulativeQuoteQty, String status, String timeInForce,
-                     String type, String side, ArrayList<Fill> fills) {
+                     String type, String side, JSONArray fills) {
         super(symbol, orderId, orderListId, clientOrderId, transactTime, price, origQty, executedQty, cummulativeQuoteQty,
                 status, timeInForce, type, side);
-        this.fills = fills;
+        loadFills(fills);
+    }
+
+    private void loadFills(JSONArray fillsArray) {
+        fills = new ArrayList<>();
+        for(int j=0; j < fillsArray.length(); j++){
+            JSONObject fill = fillsArray.getJSONObject(j);
+            fills.add(new Fill(fill.getDouble("price"),
+                    fill.getDouble("qty"),
+                    fill.getDouble("commission"),
+                    fill.getString("commissionAsset"),
+                    fill.getLong("tradeId")
+            ));
+        }
     }
 
     public ArrayList<Fill> getFills() {
         return fills;
     }
 
+    public Fill getFill(int index){
+        return fills.get(index);
+    }
+
     public static class Fill {
-            
+
+        private final double price;
+        private final double qty;
+        private final double commission;
+        private final String commissionAsset;
+        private final long tradeId;
+
+        public Fill(double price, double qty, double commission, String commissionAsset, long tradeId) {
+            this.price = price;
+            this.qty = qty;
+            this.commission = commission;
+            this.commissionAsset = commissionAsset;
+            this.tradeId = tradeId;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public double getQty() {
+            return qty;
+        }
+
+        public double getCommission() {
+            return commission;
+        }
+
+        public String getCommissionAsset() {
+            return commissionAsset;
+        }
+
+        public long getTradeId() {
+            return tradeId;
+        }
+
     }
 
 }
