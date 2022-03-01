4a3e0910e2614e1673aeb0bedb0976836ad627e4
N7ghtm4r3 <manuel033689@gmail.com>
2022-03-01 20:06:17 +0100

    Load first files

--- src/main/java/com/tecknobit/binancemanager/Managers/SignedManagers/Trade/Margin/Records/Orders/Cancel/OpenMarginOrders.java
+++ src/main/java/com/tecknobit/binancemanager/Managers/SignedManagers/Trade/Margin/Records/Orders/Cancel/OpenMarginOrders.java
@@ -1,6 +1,32 @@
 package com.tecknobit.binancemanager.Managers.SignedManagers.Trade.Margin.Records.Orders.Cancel;
 
+import java.util.ArrayList;
+
 public class OpenMarginOrders {
 
-    
+    private final ArrayList<CancelMarginOrder> cancelMarginOrders;
+    private final ArrayList<ComposedMarginOrderDetails> composedMarginOrderDetails;
+
+    public OpenMarginOrders(ArrayList<CancelMarginOrder> cancelMarginOrders,
+                            ArrayList<ComposedMarginOrderDetails> composedMarginOrderDetails) {
+        this.cancelMarginOrders = cancelMarginOrders;
+        this.composedMarginOrderDetails = composedMarginOrderDetails;
+    }
+
+    public ArrayList<CancelMarginOrder> getCancelMarginOrdersList() {
+        return cancelMarginOrders;
+    }
+
+    public CancelMarginOrder getCancelMarginOrder(int index){
+        return cancelMarginOrders.get(index);
+    }
+
+    public ArrayList<ComposedMarginOrderDetails> getComposedMarginOrderDetails() {
+        return composedMarginOrderDetails;
+    }
+
+    public ComposedMarginOrderDetails getComposedMarginOrderDetails(int index){
+        return composedMarginOrderDetails.get(index);
+    }
+
 }
