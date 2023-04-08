aaead015aba9c2ad8eae720a72dad72adaef7a65
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2023-04-08 14:29:05 +0200

    CryptoLoans

--- src/main/java/com/tecknobit/binancemanager/managers/signedmanagers/cryptoloans/BinanceCryptoLoansManager.java
+++ src/main/java/com/tecknobit/binancemanager/managers/signedmanagers/cryptoloans/BinanceCryptoLoansManager.java
@@ -3,8 +3,15 @@ package com.tecknobit.binancemanager.managers.signedmanagers.cryptoloans;
 import com.tecknobit.binancemanager.exceptions.SystemException;
 import com.tecknobit.binancemanager.managers.BinanceManager;
 import com.tecknobit.binancemanager.managers.signedmanagers.BinanceSignedManager;
+import com.tecknobit.binancemanager.managers.signedmanagers.cryptoloans.records.*;
+import com.tecknobit.binancemanager.managers.signedmanagers.cryptoloans.records.CryptoLoanAdjustLTV.LoanAdjustDirection;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.tecknobit.binancemanager.managers.BinanceManager.ReturnFormat.LIBRARY_OBJECT;
 
 /**
  * The {@code BinanceCryptoLoansManager} class is useful to manage crypto loans endpoints
@@ -149,5 +156,367 @@ public class BinanceCryptoLoansManager extends BinanceSignedManager {
         super();
     }
 
-    
+    public ArrayList<CryptoLoanIncome> getCryptoLoansIncomeHistory() throws Exception {
+        return getCryptoLoansIncomeHistory(LIBRARY_OBJECT);
+    }
+
+    public <T> T getCryptoLoansIncomeHistory(ReturnFormat format) throws Exception {
+        return getCryptoLoansIncomeHistory(null, format);
+    }
+
+    public ArrayList<CryptoLoanIncome> getCryptoLoansIncomeHistory(Params extraParams) throws Exception {
+        return getCryptoLoansIncomeHistory(extraParams, LIBRARY_OBJECT);
+    }
+
+    public <T> T getCryptoLoansIncomeHistory(Params extraParams, ReturnFormat format) throws Exception {
+        if (extraParams == null)
+            extraParams = new Params();
+        extraParams.addParam("timestamp", getServerTime());
+        String historyResponse = sendGetSignedRequest(LOAN_INCOME_ENDPOINT, extraParams);
+        switch (format) {
+            case JSON:
+                return (T) new JSONArray(historyResponse);
+            case LIBRARY_OBJECT:
+                ArrayList<CryptoLoanIncome> incomes = new ArrayList<>();
+                JSONArray jIncomes = new JSONArray(historyResponse);
+                for (int j = 0; j < jIncomes.length(); j++)
+                    incomes.add(new CryptoLoanIncome(jIncomes.getJSONObject(j)));
+                return (T) incomes;
+            default:
+                return (T) historyResponse;
+        }
+    }
+
+    public CryptoLoanBorrow execCryptoLoanBorrow(String loanCoin, String collateralCoin, int loanTerm) throws Exception {
+        return execCryptoLoanBorrow(loanCoin, collateralCoin, loanTerm, LIBRARY_OBJECT);
+    }
+
+    public <T> T execCryptoLoanBorrow(String loanCoin, String collateralCoin, int loanTerm,
+                                      ReturnFormat format) throws Exception {
+        return execCryptoLoanBorrow(loanCoin, collateralCoin, loanTerm, null, format);
+    }
+
+    public CryptoLoanBorrow execCryptoLoanBorrow(String loanCoin, String collateralCoin, int loanTerm,
+                                                 Params extraParams) throws Exception {
+        return execCryptoLoanBorrow(loanCoin, collateralCoin, loanTerm, extraParams, LIBRARY_OBJECT);
+    }
+
+    public <T> T execCryptoLoanBorrow(String loanCoin, String collateralCoin, int loanTerm, Params extraParams,
+                                      ReturnFormat format) throws Exception {
+        Params payload = createCollateralPayload(loanCoin, collateralCoin);
+        if (extraParams == null)
+            extraParams = payload;
+        else
+            extraParams.mergeParams(payload);
+        extraParams.addParam("loanTerm", loanTerm);
+        String borrowResponse = sendPostRequest(LOAN_BORROW_ENDPOINT, extraParams, apiKey);
+        switch (format) {
+            case JSON:
+                return (T) new JSONObject(borrowResponse);
+            case LIBRARY_OBJECT:
+                return (T) new CryptoLoanBorrow(new JSONObject(borrowResponse));
+            default:
+                return (T) borrowResponse;
+        }
+    }
+
+    public CryptoLoanBorrowHistory getLoanBorrowHistory() throws Exception {
+        return getLoanBorrowHistory(LIBRARY_OBJECT);
+    }
+
+    public <T> T getLoanBorrowHistory(ReturnFormat format) throws Exception {
+        return getLoanBorrowHistory(null, format);
+    }
+
+    public CryptoLoanBorrowHistory getLoanBorrowHistory(Params extraParams) throws Exception {
+        return getLoanBorrowHistory(extraParams, LIBRARY_OBJECT);
+    }
+
+    public <T> T getLoanBorrowHistory(Params extraParams, ReturnFormat format) throws Exception {
+        if (extraParams == null)
+            extraParams = new Params();
+        extraParams.addParam("timestamp", getServerTime());
+        String historyResponse = sendGetRequest(LOAN_BORROW_HISTORY_ENDPOINT, extraParams, apiKey);
+        switch (format) {
+            case JSON:
+                return (T) new JSONObject(historyResponse);
+            case LIBRARY_OBJECT:
+                return (T) new CryptoLoanBorrowHistory(new JSONObject(historyResponse));
+            default:
+                return (T) historyResponse;
+        }
+    }
+
+    public CryptoLoanOngoingOrders getLoanOngoingOrders() throws Exception {
+        return getLoanOngoingOrders(LIBRARY_OBJECT);
+    }
+
+    public <T> T getLoanOngoingOrders(ReturnFormat format) throws Exception {
+        return getLoanOngoingOrders(null, format);
+    }
+
+    public CryptoLoanOngoingOrders getLoanOngoingOrders(Params extraParams) throws Exception {
+        return getLoanOngoingOrders(extraParams, LIBRARY_OBJECT);
+    }
+
+    public <T> T getLoanOngoingOrders(Params extraParams, ReturnFormat format) throws Exception {
+        if (extraParams == null)
+            extraParams = new Params();
+        extraParams.addParam("timestamp", getServerTime());
+        String listResponse = sendGetRequest(LOAN_ONGOING_ORDERS_ENDPOINT, extraParams, apiKey);
+        switch (format) {
+            case JSON:
+                return (T) new JSONObject(listResponse);
+            case LIBRARY_OBJECT:
+                return (T) new CryptoLoanOngoingOrders(new JSONObject(listResponse));
+            default:
+                return (T) listResponse;
+        }
+    }
+
+    public CryptoLoanRepay execCryptoLoanRepay(long orderId, double amount) throws Exception {
+        return execCryptoLoanRepay(orderId, amount, LIBRARY_OBJECT);
+    }
+
+    public <T> T execCryptoLoanRepay(long orderId, double amount, ReturnFormat format) throws Exception {
+        return execCryptoLoanRepay(orderId, amount, null, format);
+    }
+
+    public CryptoLoanRepay execCryptoLoanRepay(long orderId, double amount, Params extraParams) throws Exception {
+        return execCryptoLoanRepay(orderId, amount, extraParams, LIBRARY_OBJECT);
+    }
+
+    public <T> T execCryptoLoanRepay(long orderId, double amount, Params extraParams, ReturnFormat format) throws Exception {
+        Params payload = createLoanPayload(orderId, amount);
+        if (extraParams == null)
+            extraParams = payload;
+        else
+            extraParams.mergeParams(payload);
+        String repayResponse = sendPostRequest(LOAN_REPAY_ENDPOINT, extraParams, apiKey);
+        switch (format) {
+            case JSON:
+                return (T) new JSONObject(repayResponse);
+            case LIBRARY_OBJECT:
+                return (T) new CryptoLoanRepay(new JSONObject(repayResponse));
+            default:
+                return (T) repayResponse;
+        }
+    }
+
+    public CryptoLoanRepaymentHistory getLoanRepaymentHistory() throws Exception {
+        return getLoanRepaymentHistory(LIBRARY_OBJECT);
+    }
+
+    public <T> T getLoanRepaymentHistory(ReturnFormat format) throws Exception {
+        return getLoanRepaymentHistory(null, format);
+    }
+
+    public CryptoLoanRepaymentHistory getLoanRepaymentHistory(Params extraParams) throws Exception {
+        return getLoanRepaymentHistory(extraParams, LIBRARY_OBJECT);
+    }
+
+    public <T> T getLoanRepaymentHistory(Params extraParams, ReturnFormat format) throws Exception {
+        if (extraParams == null)
+            extraParams = new Params();
+        extraParams.addParam("timestamp", getServerTime());
+        String historyResponse = sendGetRequest(LOAN_REPAY_HISTORY_ENDPOINT, extraParams, apiKey);
+        switch (format) {
+            case JSON:
+                return (T) new JSONObject(historyResponse);
+            case LIBRARY_OBJECT:
+                return (T) new CryptoLoanRepaymentHistory(new JSONObject(historyResponse));
+            default:
+                return (T) historyResponse;
+        }
+    }
+
+    public CryptoLoanAdjustLTV execCryptoLoanAdjustLTV(long orderId, double amount,
+                                                       LoanAdjustDirection direction) throws Exception {
+        return execCryptoLoanAdjustLTV(orderId, amount, direction, LIBRARY_OBJECT);
+    }
+
+    public <T> T execCryptoLoanAdjustLTV(long orderId, double amount, LoanAdjustDirection direction,
+                                         ReturnFormat format) throws Exception {
+        return execCryptoLoanAdjustLTV(orderId, amount, direction, -1, format);
+    }
+
+    public CryptoLoanAdjustLTV execCryptoLoanAdjustLTV(long orderId, double amount, LoanAdjustDirection direction,
+                                                       long recvWindow) throws Exception {
+        return execCryptoLoanAdjustLTV(orderId, amount, direction, recvWindow, LIBRARY_OBJECT);
+    }
+
+    public <T> T execCryptoLoanAdjustLTV(long orderId, double amount, LoanAdjustDirection direction, long recvWindow,
+                                         ReturnFormat format) throws Exception {
+        Params payload = createLoanPayload(orderId, amount);
+        payload.addParam("direction", direction);
+        if (recvWindow != -1)
+            payload.addParam("recvWindow", recvWindow);
+        String adjustResponse = sendPostRequest(LOAN_ADJUST_LTV_ENDPOINT, payload, apiKey);
+        switch (format) {
+            case JSON:
+                return (T) new JSONObject(adjustResponse);
+            case LIBRARY_OBJECT:
+                return (T) new CryptoLoanAdjustLTV(new JSONObject(adjustResponse));
+            default:
+                return (T) adjustResponse;
+        }
+    }
+
+    private Params createLoanPayload(long orderId, double amount) {
+        Params payload = new Params();
+        payload.addParam("orderId", orderId);
+        payload.addParam("amount", amount);
+        payload.addParam("timestamp", getServerTime());
+        return payload;
+    }
+
+    public LoanLTVAdjustmentHistory getLoanLTVAdjustmentHistory() throws Exception {
+        return getLoanLTVAdjustmentHistory(LIBRARY_OBJECT);
+    }
+
+    public <T> T getLoanLTVAdjustmentHistory(ReturnFormat format) throws Exception {
+        return getLoanLTVAdjustmentHistory(null, format);
+    }
+
+    public CryptoLoanRepaymentHistory getLoanLTVAdjustmentHistory(Params extraParams) throws Exception {
+        return getLoanLTVAdjustmentHistory(extraParams, LIBRARY_OBJECT);
+    }
+
+    public <T> T getLoanLTVAdjustmentHistory(Params extraParams, ReturnFormat format) throws Exception {
+        if (extraParams == null)
+            extraParams = new Params();
+        extraParams.addParam("timestamp", getServerTime());
+        String historyResponse = sendGetRequest(LOAN_ADJUSTMENT_HISTORY_ENDPOINT, extraParams, apiKey);
+        switch (format) {
+            case JSON:
+                return (T) new JSONObject(historyResponse);
+            case LIBRARY_OBJECT:
+                return (T) new LoanLTVAdjustmentHistory(new JSONObject(historyResponse));
+            default:
+                return (T) historyResponse;
+        }
+    }
+
+    public LoanableAssetsData getLoanableAssetsData() throws Exception {
+        return getLoanableAssetsData(LIBRARY_OBJECT);
+    }
+
+    public <T> T getLoanableAssetsData(ReturnFormat format) throws Exception {
+        return getLoanableAssetsData(null, format);
+    }
+
+    public LoanableAssetsData getLoanableAssetsData(Params extraParams) throws Exception {
+        return getLoanableAssetsData(extraParams, LIBRARY_OBJECT);
+    }
+
+    public <T> T getLoanableAssetsData(Params extraParams, ReturnFormat format) throws Exception {
+        if (extraParams == null)
+            extraParams = new Params();
+        extraParams.addParam("timestamp", getServerTime());
+        String listResponse = sendGetRequest(LOAN_LOANABLE_DATA_ENDPOINT, extraParams, apiKey);
+        switch (format) {
+            case JSON:
+                return (T) new JSONObject(listResponse);
+            case LIBRARY_OBJECT:
+                return (T) new LoanableAssetsData(new JSONObject(listResponse));
+            default:
+                return (T) listResponse;
+        }
+    }
+
+    public CollateralAssetsData getCollateralAssetsData() throws Exception {
+        return getCollateralAssetsData(LIBRARY_OBJECT);
+    }
+
+    public <T> T getCollateralAssetsData(ReturnFormat format) throws Exception {
+        return getCollateralAssetsData(null, format);
+    }
+
+    public CollateralAssetsData getCollateralAssetsData(Params extraParams) throws Exception {
+        return getCollateralAssetsData(extraParams, LIBRARY_OBJECT);
+    }
+
+    public <T> T getCollateralAssetsData(Params extraParams, ReturnFormat format) throws Exception {
+        if (extraParams == null)
+            extraParams = new Params();
+        extraParams.addParam("timestamp", getServerTime());
+        String listResponse = sendGetRequest(LOAN_COLLATERAL_DATA_ENDPOINT, extraParams, apiKey);
+        switch (format) {
+            case JSON:
+                return (T) new JSONObject(listResponse);
+            case LIBRARY_OBJECT:
+                return (T) new CollateralAssetsData(new JSONObject(listResponse));
+            default:
+                return (T) listResponse;
+        }
+    }
+
+    public CollateralRepayRate checkCollateralRepayRate(String loanCoin, String collateralCoin,
+                                                        double repayAmount) throws Exception {
+        return checkCollateralRepayRate(loanCoin, collateralCoin, repayAmount, LIBRARY_OBJECT);
+    }
+
+    public <T> T checkCollateralRepayRate(String loanCoin, String collateralCoin, double repayAmount,
+                                          ReturnFormat format) throws Exception {
+        return checkCollateralRepayRate(loanCoin, collateralCoin, repayAmount, -1, format);
+    }
+
+    public CollateralRepayRate checkCollateralRepayRate(String loanCoin, String collateralCoin, double repayAmount,
+                                                        long recvWindow) throws Exception {
+        return checkCollateralRepayRate(loanCoin, collateralCoin, repayAmount, recvWindow, LIBRARY_OBJECT);
+    }
+
+    public <T> T checkCollateralRepayRate(String loanCoin, String collateralCoin, double repayAmount, long recvWindow,
+                                          ReturnFormat format) throws Exception {
+        Params payload = createCollateralPayload(loanCoin, collateralCoin);
+        payload.addParam("repayAmount", repayAmount);
+        if (recvWindow != -1)
+            payload.addParam("recvWindow", recvWindow);
+        String repayRateResponse = sendGetRequest(LOAN_REPAY_COLLATERAL_RATE_ENDPOINT, payload, apiKey);
+        switch (format) {
+            case JSON:
+                return (T) new JSONObject(repayRateResponse);
+            case LIBRARY_OBJECT:
+                return (T) new CollateralRepayRate(new JSONObject(repayRateResponse));
+            default:
+                return (T) repayRateResponse;
+        }
+    }
+
+    private Params createCollateralPayload(String loanCoin, String collateralCoin) {
+        Params payload = new Params();
+        payload.addParam("loanCoin", loanCoin);
+        payload.addParam("collateralCoin", collateralCoin);
+        payload.addParam("timestamp", getServerTime());
+        return payload;
+    }
+
+    public CryptoLoanCustomizeMarginCall customizeMarginCall(double marginCall) throws Exception {
+        return customizeMarginCall(marginCall, LIBRARY_OBJECT);
+    }
+
+    public <T> T customizeMarginCall(double marginCall, ReturnFormat format) throws Exception {
+        return customizeMarginCall(marginCall, null, format);
+    }
+
+    public CryptoLoanCustomizeMarginCall customizeMarginCall(double marginCall, Params extraParams) throws Exception {
+        return customizeMarginCall(marginCall, extraParams, LIBRARY_OBJECT);
+    }
+
+    public <T> T customizeMarginCall(double marginCall, Params extraParams, ReturnFormat format) throws Exception {
+        if (extraParams == null)
+            extraParams = new Params();
+        extraParams.addParam("marginCall", marginCall);
+        extraParams.addParam("timestamp", getServerTime());
+        String marginCallResponse = sendPostRequest(LOAN_CUSTOMIZE_MARGIN_CALL_ENDPOINT, extraParams, apiKey);
+        switch (format) {
+            case JSON:
+                return (T) new JSONObject(marginCallResponse);
+            case LIBRARY_OBJECT:
+                return (T) new CryptoLoanCustomizeMarginCall(new JSONObject(marginCallResponse));
+            default:
+                return (T) marginCallResponse;
+        }
+    }
+
 }
--- src/main/java/com/tecknobit/binancemanager/managers/signedmanagers/cryptoloans/records/CollateralRepayRate.java
+++ src/main/java/com/tecknobit/binancemanager/managers/signedmanagers/cryptoloans/records/CollateralRepayRate.java
@@ -4,30 +4,37 @@ import com.tecknobit.binancemanager.managers.records.loan.LoanBaseRepayStructure
 import com.tecknobit.binancemanager.managers.records.loan.LoanBaseStructure;
 import org.json.JSONObject;
 
-public class CollateralRepayRate extends LoanBaseRepayStructure {
+public class CollateralRepayRate extends LoanBaseStructure {
 
+    private final double repayAmount;
     private final double rate;
 
     /**
      * Constructor to init {@link LoanBaseStructure} object
      *
      * @param loanCoin       :       coin of the vip loan
+     * @param repayAmount:
      * @param collateralCoin : collateral coin of the loan
-     * @param repayStatus    :    repay status of the loan
      **/
-    public CollateralRepayRate(String loanCoin, String collateralCoin, RepayStatus repayStatus, double rate) {
-        super(loanCoin, collateralCoin, repayStatus);
+    public CollateralRepayRate(String loanCoin, String collateralCoin, double repayAmount, double rate) {
+        super(loanCoin, collateralCoin);
+        this.repayAmount = repayAmount;
         this.rate = rate;
     }
 
     /**
      * Constructor to init {@link LoanBaseRepayStructure} object
      *
-     * @param jVIPLoanRepayStructure : loan repay structure details as {@link JSONObject}
+     * @param jCollateralRepayRate : loan repay structure details as {@link JSONObject}
      **/
-    public CollateralRepayRate(JSONObject jVIPLoanRepayStructure) {
-        super(jVIPLoanRepayStructure);
+    public CollateralRepayRate(JSONObject jCollateralRepayRate) {
+        super(jCollateralRepayRate);
+        repayAmount = hItem.getDouble("repayAmount", 0);
         rate = hItem.getDouble("rate", 0);
+    }
+
+    public double getRepayAmount() {
+        return repayAmount;
     }
 
     public double getRate() {
